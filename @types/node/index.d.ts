c29407cb14042ea5a2d2f54deb5fddb5a774c245
Max Stankevich <max.stankevich.me@gmail.com>
2019-08-06 15:21:36 +0300

    fix: root types refs

--- @types/node/index.d.ts
+++ @types/node/index.d.ts
@@ -1,4 +1,4 @@
-/// <reference path="../../../node_modules/@types/node/index.d.ts" />
+/// <reference path="../../node_modules/@types/node/index.d.ts" />
 
 declare namespace NodeJS {
   export namespace Module {

9ab54bdff9509b1d05694301e15628fb0ae239b1
Max Stankevich <max.stankevich.me@gmail.com>
2019-08-17 04:14:52 +0300

    feat: more common options

--- packages/semantic-release-ws/src/types.d.ts
+++ packages/semantic-release-ws/src/types.d.ts
@@ -1,4 +1,4 @@
-import { Branch, Commit, Options, Package, Plugins, Release, ReleaseNotes } from "@wrench/semantic-release";
+import { Branch, Commit, Options, Package, Plugins, Release, ReleaseNotes, Step } from "@wrench/semantic-release";
 
 export interface CommonOptions extends Options {
   /** Whether to update git, by creating tags or pushing changes to remote. */
@@ -28,6 +28,18 @@ export interface CommonOptions extends Options {
    * @see {@link https://www.npmjs.com/package/@semantic-release/npm @semantic-release/npm}
    */
   npmPublish: boolean;
+
+  /**
+   * Directory path to publish.
+   * @see {@link https://www.npmjs.com/package/@semantic-release/npm @semantic-release/npm}
+   */
+  pkgRoot: string;
+
+  /**
+   * Directory path in which to write the the package tarball.
+   * @see {@link https://www.npmjs.com/package/@semantic-release/npm @semantic-release/npm}
+   */
+  tarballDir: string;
 }
 
 export type WorkspaceExecHookType = "pre" | "post";

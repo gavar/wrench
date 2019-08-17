4441a3d6cb0b6e63504b330800e89e8afca3483a
Max Stankevich <max.stankevich.me@gmail.com>
2019-08-17 04:57:14 +0300

    fix: use unlinkSync since async produce race condition error

--- packages/semantic-release-npm/src/prepare.ts
+++ packages/semantic-release-npm/src/prepare.ts
@@ -1,10 +1,8 @@
 import { PrepareContext } from "@wrench/semantic-release";
-import { existsSync, unlink } from "fs";
+import { existsSync, unlinkSync } from "fs";
 import { resolve } from "path";
-import { promisify } from "util";
 import { NpmConfig, readPack, resolveTarballName } from "./common";
 
-const unlinkAsync = promisify(unlink);
 const execute = require("@semantic-release/npm/lib/prepare");
 
 export async function prepare(config: NpmConfig, context: PrepareContext) {
@@ -16,7 +14,7 @@ export async function prepare(config: NpmConfig, context: PrepareContext) {
     if (existsSync(tarballPath)) {
       // BUG: `@semantic-release/npm` throws an error if tarball already exists
       context.logger.warn("removing previous tarball: %s", tarballPath);
-      await unlinkAsync(tarballPath);
+      unlinkSync(tarballPath);
     }
   }
   // BUG: run directly, verification logic of `semantic-release/npm` is broken

a473c9b78fb7e080169ff73e6a1a57ff52e1fcf0
Max Stankevich <max.stankevich.me@gmail.com>
2019-08-28 16:35:54 +0300

    feat: expose more GIT methods

--- packages/semantic-release/src/expose/git.ts
+++ packages/semantic-release/src/expose/git.ts
@@ -1,5 +1,5 @@
 import { Options } from "execa";
-import { Release } from "../types";
+import { Commit, Release } from "../types";
 
 const git = require("semantic-release/lib/git");
 
@@ -9,4 +9,12 @@ export function tag(release: Release, options?: Options): Promise<void> {
 
 export function push(url: string, options?: Options): Promise<void> {
   return git.push(url, options);
+}
+
+export function getGitHead(options: Options): Promise<string> {
+  return git.getGitHead(options);
+}
+
+export function getCommits(from: string, to: string, options: Options): Promise<Commit[]> {
+  return git.getCommits(from, to, options);
 }

5a8f970736ba48e5b459fd235f83aa4fafc7234b
Max Stankevich <max.stankevich.me@gmail.com>
2019-09-01 01:16:44 +0300

    fix: parse version channel when using last release version from package

--- packages/semantic-release-ws/src/plugin/analyze-commits/resolve-last-release.ts
+++ packages/semantic-release-ws/src/plugin/analyze-commits/resolve-last-release.ts
@@ -1,4 +1,4 @@
-import { Branch, getLastRelease, Package } from "@wrench/semantic-release";
+import { Branch, getLastRelease, getReleaseChannel, Package } from "@wrench/semantic-release";
 import { LastRelease } from "@wrench/semantic-release/src";
 
 /**
@@ -7,6 +7,9 @@ import { LastRelease } from "@wrench/semantic-release/src";
  */
 export function resolveLastRelease(branch: Branch, tagFormat: string, pack: Package): LastRelease {
   const release = getLastRelease(branch, tagFormat);
-  if (!release.version) release.version = pack.version;
+  if (!release.version) {
+    release.version = pack.version;
+    release.channel = getReleaseChannel(pack.version);
+  }
   return release;
 }

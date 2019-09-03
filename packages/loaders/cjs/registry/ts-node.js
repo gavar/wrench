1b3fb47d2ba5a4628a8ed370bbce9de6df5c1959
Max Stankevich <max.stankevich.me@gmail.com>
2019-09-04 02:45:18 +0300

    feat: checks `process.env` when creating `ts-node` options

--- packages/loaders/cjs/registry/ts-node.js
+++ packages/loaders/cjs/registry/ts-node.js
@@ -4,15 +4,30 @@
 /** @type {import("../../types").LoaderWithProps<Options>} */
 const TS_NODE_LOADER = ["ts-node", {
   func: "register",
-  args: {
+  args: args(),
+}];
+
+/**
+ * @returns {Options}
+ */
+function args() {
+  /** @type {Options} */
+  const options = {
+    // no reason to ignore as it should only resolve to ts files
+    ignore: [],
     compilerOptions: {
       declaration: false,
       declarationMap: false,
       module: "commonjs",
       noEmitHelpers: false,
     },
-  },
-}];
+  };
+
+  if (process.env.TS_NODE_IGNORE) delete options.ignore;
+  if (process.env.TS_NODE_SKIP_IGNORE) delete options.ignore;
+  if (process.env.TS_NODE_COMPILER_OPTIONS) delete options.compilerOptions;
+  return options;
+}
 
 module.exports = {
   TS_NODE_LOADER,

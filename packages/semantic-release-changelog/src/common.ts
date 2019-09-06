3d391ddd44e56882da4a3481c86ecde8bf75d498
Max Stankevich <max.stankevich.me@gmail.com>
2019-09-06 06:35:09 +0300

    fix: catch git checkout errors

--- packages/semantic-release-changelog/src/common.ts
+++ packages/semantic-release-changelog/src/common.ts
@@ -2,7 +2,6 @@ import cp from "child_process";
 import { promisify } from "util";
 import { ChangelogConfig } from "./types";
 
-// const readFileAsync = promisify(readFile);
 const exec = promisify(cp.exec);
 
 export const DEFAULT_CONFIG: Partial<ChangelogConfig> = {
@@ -10,5 +9,10 @@ export const DEFAULT_CONFIG: Partial<ChangelogConfig> = {
 };
 
 export async function checkout(hash: string, filename: string) {
-  await exec(`git checkout ${hash} -- ${filename}`);
+  try {
+    await exec(`git checkout ${hash} -- ${filename}`);
+  } catch (e) {
+    if (!e.stderr.includes("did not match any file(s) known to git"))
+      throw e;
+  }
 }
